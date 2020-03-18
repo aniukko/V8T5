638a8505a11523461d456a130688dcbf2554fc99
aniukko <aniukko@gmail.com>
2020-03-18 20:20:53 +0200

    a

---
+++ V10T1/app/src/main/java/com/example/v10t1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.v10t1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web = findViewById(R.id.webView);
+        web.setWebViewClient(new WebViewClient());
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("");
+    }
+}
